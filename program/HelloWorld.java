1f61efefc61dc363a077f4986cb8f8039c3a9f6b
julianjoep <julianjoepereira@gmail.com>
2023-07-25 09:59:03 +0530

    modified helloworld program

--- program/HelloWorld.java
+++ program/HelloWorld.java
@@ -3,5 +3,8 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Welcome to sada"); 
+        long a=10L;
+        a =  (short)a*10;
+        System.out.println(a);
     }
 }
